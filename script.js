08e0b6ee91224d9d77ee0753a8ad7980839eeb3f
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-11 19:13:30 -0700

    queryseletorAll

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const places = document.getElementsByClassName('important-places');
 for(const place of places){
     console.log(place.innerText)
 }
+
+// queryseletorAll
+const someLi = document.querySelectorAll('#fruits-title')
+console.log(someLi)
+for(const li of someLi){
+    console.log(li.innerText)
+}
